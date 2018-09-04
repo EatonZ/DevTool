b785b23bec4dc11a5ed2c7f5d9c69aa2c8d570a8
Eaton <eaton@eaton-works.com>
2018-09-03 20:16:39 -0400

    Correct GUID

--- src/DevTool/Properties/AssemblyInfo.cs
+++ src/DevTool/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Eaton Works and DevTool are either trademarks or registered trademarks of Eaton Works. Unauthorized modification, reverse engineering, and distribution is strictly forbidden and violators will be prosecuted to the maximum extent of the law.")]
 [assembly: AssemblyVersion("2.0.0.2276")]
 [assembly: ComVisible(false)]
-[assembly: Guid("5c9d6399-9e31-4167-81ef-de8c4a053882")]
+[assembly: Guid("ddaa01a2-76e3-41be-b5a5-c16ca533d469")]
